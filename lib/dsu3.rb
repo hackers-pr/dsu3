45c2b657f35ed9b9d8238ec79527fc33dfb03fbd
hackers-pr <hackers-pr@users.noreply.github.com>
2022-07-18 21:54:10 +0300

    slightly reworked props.rb

--- lib/dsu3.rb
+++ lib/dsu3.rb
@@ -5,7 +5,7 @@ require 'dsu3/core'
 
 # Main DSU3 namespace
 module DSU3
-  VERSION = '0.4'
+  VERSION = '0.5.0'
 
   LOGGER = Logger.new($stdout)
   LOGGER.level = Logger::WARN
--- lib/dsu3/props.rb
+++ lib/dsu3/props.rb
@@ -5,24 +5,34 @@ require 'json'
 require 'base64'
 
 module DSU3
+  # The fundamental class in this vast system, on which all the other parts of the structure depend
+  # pretty bad code in this module rests by the way
   module Props
     # user-agent header
-    USER_AGENT =
-      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '\
-      'discord/0.0.18 Chrome/91.0.4472.164 Electron/13.6.6 Safari/537.36'
+    USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0'
+
+    DATAMINING_COMMITS_URL = 'https://api.github.com/repos/Discord-Datamining/Discord-Datamining/commits/master'
+
+    # Fetches Discord client build number
+    def fetch_build_number
+      JSON.parse(RestClient.get(DATAMINING_COMMITS_URL))['commit']['message'].match(/Build (\d+)/)[1]
+    end
 
     # Decoded x-super-properties header
     SUPER_PROPERTIES = {
       os: 'Linux',
-      browser: 'Discord Client',
+      browser: 'Firefox',
+      device: '',
+      system_locale: 'en',
+      browser_user_agent: USER_AGENT,
+      browser_version: '91.0',
+      os_version: '',
+      referrer: '',
+      referring_domain: '',
+      referrer_current: '',
+      referring_domain_current: '',
       release_channel: 'stable',
-      client_version: '0.0.18',
-      os_version: '5.10.0-14-amd64',
-      os_arch: 'x64',
-      system_locale: 'en-US',
-      window_manager: 'XFCE,xfce',
-      distro: 'Debian GNU/Linux 11 (bullseye)',
-      client_build_number: 136_921,
+      client_build_number: fetch_build_number,
       client_event_source: nil
     }.freeze
 
@@ -32,16 +42,19 @@ module DSU3
     HEADERS = {
       accept: '*/*',
       accept_encoding: 'gzip, deflate, br',
-      accept_language: 'en-US',
+      accept_language: 'en',
+      alt_used: 'discord.com',
+      connection: 'keep-alive',
+      host: 'discord.com',
       sec_fetch_dest: 'empty',
       sec_fetch_mode: 'cors',
       sec_fetch_site: 'same-origin',
+      te: 'trailers',
       user_agent: USER_AGENT,
       x_debug_options: 'bugReporterEnabled',
       x_discord_locale: 'en-US',
       x_super_properties: Base64.strict_encode64(SUPER_PROPERTIES.to_json),
       content_type: 'application/json',
-      origin: 'https://discord.com',
       cookie: HTTP::Cookie.cookie_value(resp.cookie_jar.cookies),
       x_fingerprint: JSON.parse(resp.body)['fingerprint']
     }.freeze
